6fbd454b2c90c42f8a0c4ff764877a716604f8af
wangjuhui <13934720500@163.com>
2021-12-17 10:54:15 +0800

    文档补全

---
+++ bin/http.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const Koa = require('koa2')
+const path = require('path')
+const serve = require('koa-static')
+const app = new Koa()
+const argv = process.argv.slice(2)
+const name = argv[0]
+
+// 端口号
+const port = 10003
+// 静态资源处理
+app.use(serve(path.join(process.cwd(), name || '')))
+
+app.listen(port, () => {
+  console.log('接口指向目录', path.join(process.cwd(), name || ''))
+  console.log(`Server in running at http://localhost:${port}`)
+})
